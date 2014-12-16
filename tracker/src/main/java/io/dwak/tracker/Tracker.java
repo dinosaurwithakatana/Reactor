bcccf81a89f98b9d6a65b7e936398a7df72e2dde
Vishnu Rajeevan <vrajeevan@roundarch.com>
2014-12-16 16:28:29 -0600

    move computation to inner class

--- tracker/src/main/java/io/dwak/tracker/Tracker.java
+++ tracker/src/main/java/io/dwak/tracker/Tracker.java
@@ -32,4 +32,63 @@ public class Tracker {
         }
     }
 
+    public class TrackerComputation {
+        private final TrackerComputation sInstance;
+        private final boolean mStopped;
+        private final boolean mInvalidated;
+        private final int mId;
+        private final ArrayList<TrackerInvalidateCallbacks> mInvalidateCallbacks;
+        private final TrackerComputation mParent;
+        private final TrackerComputationFunction mFunction;
+        private final boolean mRecomputing;
+        private boolean mFirstRun;
+        private boolean mErrored;
+        private boolean mConstructingComputation = false;
+
+        TrackerComputation(TrackerComputationFunction function, TrackerComputation parent) {
+            sInstance = this;
+            mStopped = false;
+            mInvalidated = false;
+            mId = nextId++;
+            mInvalidateCallbacks = new ArrayList<TrackerInvalidateCallbacks>();
+            mFirstRun = true;
+            mParent = parent;
+            mFunction = function;
+            mRecomputing = false;
+
+            try {
+                function.onCompute();
+                mErrored = false;
+            } finally {
+                mFirstRun = false;
+                if (mErrored) {
+                    stop();
+                }
+
+            }
+        }
+
+        private void stop() {
+
+        }
+
+        private void onInvalidate() {
+
+        }
+
+        private void invalidate() {
+
+        }
+
+        private void compute() {
+
+        }
+
+        private void reCompute() {
+
+        }
+    }
+
+    private class TrackerInvalidateCallbacks {
+    }
 }
