9cd7d40dd212b3eebb6eb1e2b2fd6bf5ebbb1161
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-01-25 19:24:29 -0600

    adding docs to ReactiveVar

--- reactor/src/main/java/io/dwak/reactor/ReactorVar.java
+++ reactor/src/main/java/io/dwak/reactor/ReactorVar.java
@@ -1,6 +1,9 @@
 package io.dwak.reactor;
 
 /**
+ * Object that wraps an object and it's {@link io.dwak.reactor.ReactorDependency}
+ * If you'd like to use primitive data types, you'll have to handle your own instance of
+ * {@link io.dwak.reactor.ReactorDependency} alongside your primitive value
  * Created by vishnu on 1/25/15.
  */
 public class ReactorVar<T> {
